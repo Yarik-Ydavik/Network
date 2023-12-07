9fccdaead6158e1204c9675f970193b3bdaa608a
Yaroslav Zagumennikov <yzagumennikov1@mail.ru>
2023-12-07 15:23:39 +0500

    Добавьте файлы проекта.

---
+++ Network/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Windows.Threading;
+using System.Net.NetworkInformation;
+
+namespace Network
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer;
+        NetworkInterfaceInfo selectedInterfaceInfo;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 1) };
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdateNetworkInterface();
+        }
+
+        private void UpdateNetworkInterface()
+        {
+            if (selectedInterfaceComboBox.SelectedItem != null)
+            {
+                selectedInterfaceInfo = new NetworkInterfaceInfo((NetworkInterfaceType)selectedInterfaceComboBox.SelectedItem);
+                selectedInterfaceInfo.UpdateData();
+                interfaceInfoTextBlock.Text = selectedInterfaceInfo.ToString();
+            }
+           
+        }
+
+        private void selectedInterfaceComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (selectedInterfaceComboBox.SelectedItem != null)
+            {
+                selectedInterfaceInfo = new NetworkInterfaceInfo((NetworkInterfaceType)selectedInterfaceComboBox.SelectedItem);
+                selectedInterfaceInfo.UpdateData();
+                interfaceInfoTextBlock.Text = selectedInterfaceInfo.ToString();
+            }
+        }
+
+    }
+}
---
+++ Network/NetworkInterfaceInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.NetworkInformation;
+
+public class NetworkInterfaceInfo
+{
+    private NetworkInterfaceType interfaceType;
+    private long maxSpeed;
+    private long bytesSent;
+    private long bytesReceived;
+
+    public NetworkInterfaceInfo(NetworkInterfaceType type)
+    {
+        interfaceType = type;
+    }
+
+    public void UpdateData()
+    {
+        NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+        foreach (NetworkInterface networkInterface in interfaces)
+        {
+            if (networkInterface.NetworkInterfaceType == interfaceType)
+            {
+                maxSpeed = networkInterface.Speed;
+                bytesSent = networkInterface.GetIPv4Statistics().BytesSent;
+                bytesReceived = networkInterface.GetIPv4Statistics().BytesReceived;
+                break;
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Interface Type: {interfaceType}\nMax Speed: {maxSpeed} bps\nBytes Sent: {bytesSent}\nBytes Received: {bytesReceived}";
+    }
+}
